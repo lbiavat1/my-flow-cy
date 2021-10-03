f76ee7d6e69d6c874161e332c8969467b8548e9e
Luca Biavati <lbiavat1@jh.edu>
2021-10-03 18:27:04 -0400

    install devtools

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # my-flow-cy
 analysis of high dimensional single-cell cytometry data
+
+# Part 1
+install packages
---
+++ setup_install_packages.R
@@ -0,0 +1,8 @@
+rm(list = ls())
+
+if(!require("devtools"))
+  install.packages("devtools")
+
+# after installing devtools, restart RStudio
+
+library(devtools)
