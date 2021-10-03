6fb2570125ea4b631a57030847f529c5a804a018
Luca Biavati <lbiavat1@jh.edu>
2021-10-03 19:42:24 -0400

    install spectre issue with catalyst

--- setup_install_packages.R
+++ setup_install_packages.R
@@ -11,3 +11,19 @@ if(!require("devtools"))
 # after installing devtools, restart RStudio
 
 library(devtools)
+
+options(timeout = 6000)
+# download and install spectre 
+devtools::install_github("immunedynamics/spectre")
+
+library(Spectre)
+Spectre::package.check()
+
+if(!require("tidyverse"))
+  install.packages("tidyverse")
+
+library(tidyverse)
+
+BiocManager::install("CATALYST", dependencies = TRUE)
+
+library(CATALYST)
