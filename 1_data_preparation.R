a07bf6a37d49eb75e8439d8d2e89cfd2fab92094
Luca Biavati <lbiavat1@jh.edu>
2021-10-03 20:00:15 -0400

    set directories

--- 1_data_preparation.R
+++ 1_data_preparation.R
@@ -16,3 +16,28 @@ getwd()
 PrimaryDirectory <- getwd()
 PrimaryDirectory
 
+### Set 'input' directory
+setwd(PrimaryDirectory)
+if(!dir.exists("data"))
+  dir.create("data")
+setwd("data/")
+InputDirectory <- getwd()
+setwd(PrimaryDirectory)
+
+### Set 'metadata' directory
+setwd(PrimaryDirectory)
+if(!dir.exists("metadata"))
+  dir.create("metadata")
+setwd("metadata/")
+MetaDirectory <- getwd()
+setwd(PrimaryDirectory)
+
+### Create output directory
+if(!dir.exists("Output"))
+  dir.create("Output", showWarnings = FALSE)
+setwd("Output")
+OutputDirectory <- getwd()
+setwd(PrimaryDirectory)
+
+
+
