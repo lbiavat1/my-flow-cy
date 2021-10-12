7b559bcf5b3dba285fa9a2407479272fd4c75d6f
Luca Biavati <lbiavat1@jh.edu>
2021-10-12 16:27:22 -0400

    start metadata prep

--- 1_data_preparation.R
+++ 1_data_preparation.R
@@ -39,5 +39,23 @@ setwd("Output")
 OutputDirectory <- getwd()
 setwd(PrimaryDirectory)
 
+# import data
+list.files(InputDirectory)
+data.list <- Spectre::read.files(file.loc = InputDirectory, file.type = ".csv", 
+                                 do.embed.file.names = TRUE)
+check <- do.list.summary(data.list)
+check$name.table
+
+# merge data
+cell.data <- do.merge.files(data.list)
+
+# prep and read in metadata
+
+Filename <- list.files(InputDirectory)
+# write_csv(as.data.frame(Filename), file = paste(MetaDirectory, "sample.details.csv", sep = "/"))
+metadata_file <- paste(MetaDirectory, "sample.details.csv", sep = "/")
+sample_details <- read_csv(metadata_file)
+
+
 
 
